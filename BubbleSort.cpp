b6061b0cf2aea5e802dc5ec80c9f83e37e6274cf
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2023-08-18 21:38:18 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,6 +5,7 @@ using namespace std;
 void BubbleSort(int a[], int n) {
     // Outer loop iterates from the last index towards the first index of the array
     for (int i = n - 1; i >= 1; i--) {
+	    int swapoccur = 0;
         // Inner loop iterates through the array from the first index to (i-1) index
         for (int j = 0; j <= i - 1; j++) {
             // Comparing adjacent elements and swapping them if they are out of order
@@ -12,8 +13,12 @@ void BubbleSort(int a[], int n) {
                 int temp = a[j + 1];  // Temporary variable to hold the smaller element
                 a[j + 1] = a[j];      // Assigning the larger element to the next position
                 a[j] = temp;         // Assigning the smaller element to the current position
+		    swapoccur = 1;
             }
-        }
+		if(swapoccur == 0)
+		{
+			break;
+		}
     }
 }
 
