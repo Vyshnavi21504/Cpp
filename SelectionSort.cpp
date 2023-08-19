a7f8cfa9c61041bd8cc7aef69283453ed2fcbecb
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2023-08-19 21:50:19 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -1,3 +1,40 @@
+//Selection Sort
+#include <iostream>
+using namespace std;
+
+void selectionsort(int a[], int n) {
+    for (int i = 0; i < n - 1; i++) { // Loop through the array
+        int mini = i; // Assume the current element is the smallest
+        for (int j = i; j < n; j++) { // Loop through the unsorted portion of the array
+            if (a[mini] > a[j]) {
+                mini = j; // Update the index of the smallest element found so far
+            }
+        }
+
+        // Swap the smallest element found with the first element of the unsorted portion
+        int temp = a[mini];
+        a[mini] = a[i];
+        a[i] = temp;
+    }
+}
+
+int main() {
+    int n;
+    cin >> n; // Input the size of the array
+    int a[n];
+    for (int i = 0; i < n; i++) {
+        cin >> a[i]; // Input array elements
+    }
+    
+    selectionsort(a, n); // Call the selection sort function
+    
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << " "; // Output the sorted array
+    }
+    
+    return 0; // Indicate successful execution
+}
 //Selection Sorting
 #include <iostream>
 using namespace std;
