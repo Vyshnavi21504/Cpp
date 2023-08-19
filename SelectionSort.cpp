ebf10bfc54c117c398c394d231402242da6321db
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2023-08-19 21:50:53 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -35,30 +35,3 @@ int main() {
     
     return 0; // Indicate successful execution
 }
-//Selection Sorting
-#include <iostream>
-using namespace std;
-void Selection_sort(int a[] , int n){
-	for(int i=0;i<=n-1;i++){
-		int j = i;
-		while( j>0 && a[j-1]>a[j]){
-			int temp = a[j-1];
-			a[j-1] = a[j];
-			a[j] = temp;
-			j--;
-		}
-	}
-}
-int main(){
-	int n;
-	cin>>n;
-	int a[n];
-	for(int i=0;i<n;i++){
-		cin>>a[i];
-	}
-	Selection_sort(a,n);
-	cout<<"After Sorting :" <<endl;
-	for(int i=0;i<n;i++){
-		cout<<a[i]<<" ";
-	}
-}
