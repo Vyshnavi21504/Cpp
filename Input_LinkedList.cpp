7b82ff6e7a4a2580fb0d407c642437e4c354e7f0
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2023-09-01 00:17:00 +0530

    Update Input_LinkedList.cpp

--- Input_LinkedList.cpp
+++ Input_LinkedList.cpp
@@ -1,5 +1,63 @@
 #include<iostream>
 using namespace std;
+class Node{
+	public:
+		int data;
+		Node *next;
+		
+		Node(int data){
+			this->data = data;
+			next = NULL;
+		}
+};
+
+void print (Node *head){
+	Node *temp = head;
+	while(temp!=NULL){
+		cout<<temp->data<<" ";
+		temp = temp->next;
+	}
+}
+Node* takeinput(){
+	int data;
+	cin>>data;
+	Node *head =  NULL;    //LL is empty
+	Node *tail = NULL;     //LL is empty
+	while(data!=-1){
+		Node *n = new Node(data);   //creating LL
+		if(head == NULL){
+			head = n;
+			tail = n;
+		}else{
+			tail->next = n;  //inserting at tail
+			tail = n;
+		}
+		cin>>data;
+	}
+	return head;
+}
+Node* takeinput2(){
+	int data;
+	cin>>data;
+	Node *head = NULL;
+	Node *tail = NULL;
+	while(data!= -1){
+		Node *n = new Node(data);
+		//1st Node
+		if(head == NULL){
+			head = n;
+			tail = n;
+		}else{
+			n -> next = head;
+			head = n;
+		}
+		cin>>data;
+	}
+	return head;
+}
 int main(){
-	
+	Node *head = takeinput();
+	Node *head2 = takeinput2();
+	print (head);
+	print(head2);
 }
