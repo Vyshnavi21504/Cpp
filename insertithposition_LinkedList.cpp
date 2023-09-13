3e5e911cf3422c2ff024dbfe4d9cfd4c997088a6
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2023-09-13 18:28:17 +0530

    Add files via upload

---
+++ insertithposition_LinkedList.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int length(Node* head) {
+    int count = 0;
+    while (head) {
+        count++;
+        head = head->next;
+    }
+    return count;
+}
+
+void printIthNode(Node* head, int i) {
+    int n = length(head);
+    if (i < 0 || i >= n) {
+        cout << "-1" << endl;
+        return;
+    }
+    int count = 0;
+    while (count < i && head != NULL) {
+        head = head->next;
+        count++;
+    }
+    cout << head->data;
+}
+
+Node* insertithposition(Node* head, int i, int data) {
+    if (i < 0) {
+        return head;
+    }
+
+    if (i == 0) {
+        Node* n = new Node(data);
+        n->next = head;
+        head = n;
+        return head;
+    }
+
+    int count = 1;
+    Node* current = head;
+    while (count < i && current != NULL) {
+        current = current->next; 
+        count++;
+    }
+
+    if (current) { 
+    //METHOD-1
+//        Node* n = new Node(data);
+//        n->next = current->next;
+//        current->next = n;
+
+       //METHOD-2
+          	 if (current) { 
+        Node* n = new Node(data);
+        n->next = current->next;
+        current->next = n;
+    }
+    }
+    return head;
+}
+
+Node* takeInput() {
+    int data;
+    cin >> data;
+    Node* head = NULL;
+    Node* tail = NULL;
+    while (data != -1) {
+        Node* n = new Node(data);
+        if (head == NULL) {
+            head = n;
+            tail = n;
+        }
+        else {
+            tail->next = n;
+            tail = n;
+        }
+        cin >> data;
+    }
+    return head;
+}
+
+int main() {
+    Node* head = takeInput(); 
+    int i, data;
+    cin >> i >> data;
+    Node* copyhead = head; // Declare copyhead and initialize it with the original head
+    head = insertithposition(head, i, data); 
+    print(head);
+    return 0;
+}
