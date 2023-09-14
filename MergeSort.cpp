a1baad99c2e0ad3bd5ae12d6b6ce47946c9cd4d2
Vyshnavi Madhusudhan <111994644+Vyshnavi21504@users.noreply.github.com>
2023-09-14 21:39:05 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,56 +1,66 @@
-//code for merge sort
+#include <iostream>
+#include <vector>
 
-//code for merge sort
-
-#include<iostream>
-#include<vector>
 using namespace std;
 
+void merge(vector<int>& arr, int low, int mid, int high) {
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+    
+    // Merge the two halves into temp array
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        } else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
 
-void merge(vector<int>& arr, int low, int mid, int high){
-	vector<int>temp;
-	int left = low;
-	int right = mid+1;
-	
-	while(left <= mid && right <= high){
-		if(arr[left] <= arr[right]){
-			temp.push_back(arr[left]);
-			left++;
-		}else{
-			temp.push_back(arr[right]);
-			right++;
-		}
-	}
-	while(left <= mid){
-		temp.push_back(arr[left]);
-			left++;
-	}
-	while(right<=high){
-		temp.push_back(arr[right]);
-			right++;
-	}
-	for(int i=low ;i<=high;i++){
-		arr[i] = temp[i-low];
-	}
+    // Copy the remaining elements from left subarray
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    // Copy the remaining elements from right subarray
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // Copy sorted elements from temp back to the original array
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
 }
 
+void mergeSort(vector<int>& arr, int low, int high) {
+    if (low < high) {
+        int mid = (low + high) / 2;
 
-void mergeSort(vector<int>&arr ,int low ,int high){
-	if(low >= high) return;
-	int mid = (low+high)/2;
-	mergeSort(arr,low,mid);
-	mergeSort(arr,mid+1,high);
-	merge(arr,low,mid,high);
+        // Recursively sort the left and right halves
+        mergeSort(arr, low, mid);
+        mergeSort(arr, mid + 1, high);
+
+        // Merge the sorted halves
+        merge(arr, low, mid, high);
+    }
 }
-int main(){
-	vector<int> arr  = {4 ,3 ,2 ,1 ,5 ,6 , 7};
-	int n = 7;
+
+int main() {
+    vector<int> arr = {12, 11, 13, 5, 6, 7};
+    int n = arr.size();
 
     mergeSort(arr, 0, n - 1);
-	
-	for(int i=0;i<n;i++){
-		cout<<arr[i]<<" ";
-	}
-	cout<<endl;
-	return 0;
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
